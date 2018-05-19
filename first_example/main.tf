5b19c57b29092f8553b69baf6015a217136ef4ae
JacobPimental <jacob16682@gmail.com>
2018-05-18 22:08:12 -0400

    Finished Variable Example

--- first_example/main.tf
+++ first_example/main.tf
@@ -3,13 +3,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+  type = "string"
+}
+
+output "public_ip" {
+	description = "Public IP of web server"
+	value = "${aws_instance.example.public_ip}"
+}
+
 #Creates security group that allows server to listen on port 8080
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = "${var.server_port}" 
+    to_port = "${var.server_port}"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -24,7 +35,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
     #!/bin/bash
     echo "Hello, World" > index.html
-    nohup busybox httpd -f -p 8080 &
+    nohup busybox httpd -f -p "${var.server_port}" &
     EOF
   
   tags {
